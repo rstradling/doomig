368d4bd426fca7ef2b4244bca33e276328bcd85e
Ryan Stradling <rstradling@insiderealestate.com>
2024-02-12 14:01:04 -0500

    Updating some tests

--- core/src/main/scala/com/strad/doomig/service/VersionStampDbService.scala
+++ core/src/main/scala/com/strad/doomig/service/VersionStampDbService.scala
@@ -1,13 +1,13 @@
 package com.strad.doomig.service
 
 import cats.*
-import cats.implicits.*
-import cats.syntax.*
 import cats.effect.*
+import cats.implicits.*
 import com.strad.doomig.domain.Migration
 import doobie.util.transactor.Transactor
 import doobie.*
 import doobie.implicits.*
+
 object VersionStampDbService:
   def apply[F[_]: Async](db: Transactor[F]): VersionStampService[F, String] =
     new VersionStampService[F, String]:
@@ -23,23 +23,32 @@ object VersionStampDbService:
           .transact(db)
 
       override def dropTable(tableName: String): F[Int] =
-        sql"""DROP TABLE $tableName;""".update.run.transact(db)
+        val s = s"""DROP TABLE IF EXISTS $tableName;"""
+        Update[Unit](s, None).toUpdate0(()).run.transact(db)
 
       override def createTable(tableName: String): F[Int] =
-        sql"""CREATE TABLE ${tableName} (
+        val s =
+          s"""CREATE TABLE $tableName (
              version varchar(100) PRIMARY KEY,
              name varchar(250),
-             description varchar(250)
-           )""".update.run
-          .transact(db)
+             description varchar(250));
+             """
+
+        Update[Unit](s, None).toUpdate0(()).run.transact(db)
 
       override def fetchCurrentVersion(tableName: String): F[Option[Migration[String]]] =
-        sql"""SELECT version, name, description FROM ${tableName}"""
-          .query[Migration[String]]
-          .option
-          .transact(db)
+        val exists = doesTableExist(tableName)
+        exists.flatMap { e =>
+          if e then
+            val select = fr"""SELECT version, name, description FROM""" ++ Fragment.const(tableName)
+            select
+              .query[Migration[String]]
+              .option
+              .transact(db)
+          else Async[F].pure(None)
+        }
 
       override def writeVersion(tableName: String, migration: Migration[String]): F[Int] =
-        sql"""INSERT INTO ${tableName} 
-             VALUES (${migration.version}, ${migration.name}, ${migration.description});""".update.run
-          .transact(db)
+        val s = fr"""INSERT INTO""" ++ Fragment.const(tableName)
+        val v = fr"""VALUES (${migration.version}, ${migration.name}, ${migration.description});"""
+        (s ++ v).update.run.transact(db)
---
+++ integration/src/test/scala/com/strad/doomig/integration/service/VersionStampDbServiceSpec.scala
@@ -0,0 +1,47 @@
+package com.strad.doomig.integration.service
+
+import cats.effect.*
+import cats.implicits.*
+import com.strad.doomig.domain.Migration
+import com.strad.doomig.service.{VersionStampDbService, VersionStampService}
+import doobie.Transactor
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
+import fs2.Stream
+import munit.CatsEffectSuite
+
+class VersionStampDbServiceSpec extends CatsEffectSuite:
+  def mkDbConnection(
+    host: String,
+    port: String,
+    database: String,
+    user: String,
+    password: String
+  ): Resource[IO, Transactor[IO]] =
+    for
+      ce <- ExecutionContexts.fixedThreadPool[IO](5)
+      url = s"jdbc:postgresql://${host}:${port}/${database}"
+      xa <- HikariTransactor.newHikariTransactor[IO]("org.postgresql.Driver", url, user, password, ce)
+    yield xa
+  test("CRUD operations work as intended"):
+    def run[A](tableName: String)(fn: String => IO[A]): A =
+      (for res <- Resource.eval(fn(tableName))
+      yield res).allocated.unsafeRunSync()._1
+    def run2[A](tableName: String, migration: Migration[String])(fn: (String, Migration[String]) => IO[A]): A =
+      (for res <- Resource.eval(fn(tableName, migration))
+      yield res).allocated.unsafeRunSync()._1
+
+    val repo = mkDbConnection("localhost", "5432", "testing", "admin", "test123")
+      .map(VersionStampDbService[IO](_))
+      .allocated
+      .unsafeRunSync()
+      ._1
+    run("foo")(repo.dropTable)
+    assertEquals(run("foo")(repo.doesTableExist), false)
+    assertEquals(run("foo")(repo.createTable), 0)
+    assertEquals(run("foo")(repo.doesTableExist), true)
+    val migration = Migration[String]("100", "Testing", "Testing code")
+    assertEquals(run2[Int]("foo", Migration[String]("100", "Testing", "Testing code"))(repo.writeVersion), 1)
+    assertEquals(run("foo")(repo.fetchCurrentVersion), migration.some)
+    assertEquals(run("foo")(repo.dropTable), 0)
+    assertEquals(run("foo")(repo.fetchCurrentVersion), None)
