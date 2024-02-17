99747718d395e4b3575f84db121d130a2399ba58
ryanstradling <ryanstradling@gmail.com>
2024-02-17 11:52:13 -0500

    Closes #11 Adds abililty to specify a versionStamper that is not hardcoded to postgresql even though that is the only supported version

--- core/src/main/scala/com/strad/doomig/db/VersionStampRepo.scala
+++ core/src/main/scala/com/strad/doomig/db/VersionStampRepo.scala
@@ -1,6 +1,8 @@
 package com.strad.doomig.db
 
+import cats.effect.Async
 import com.strad.doomig.domain.{Dao, Svc}
+import doobie.util.transactor.Transactor
 
 trait VersionStampRepo[F[_]]:
   def doesTableExist(tableName: String): F[Boolean]
@@ -9,3 +11,9 @@ trait VersionStampRepo[F[_]]:
   def deleteVersion(tableName: String, version: String): F[Int]
   def createTableIfNotExist(tableName: String): F[Int]
   def dropTableIfExists(tableName: String): F[Int]
+
+object VersionStampRepo:
+  def apply[F[_]: Async](driver: String, db: Transactor[F]) =
+    driver.toLowerCase() match
+      case "org.postgresql.driver" => VersionStampPostgresRepo(db)
+      case _                       => throw new RuntimeException("Unsupported driver")
